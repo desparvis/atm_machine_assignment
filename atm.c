8ac8937377a879c2d3b71619b3a7c99bb3be8bc1
desparvis <latifahm501@gmail.com>
2025-09-19 17:50:31 +0200

    'code_readability_improvement'

--- atm.c
+++ atm.c
@@ -25,33 +25,41 @@ int withdraw(int amount, int bal)
 // Function that runs the whole program
 int main()
 {
+
     // Declaring all needed variables
     int balance = 0, amount;
     char ins;
+
     // Making a loop that keeps running the program until the user exits
     while (1)
     {   
+
         // Making a welcome menu that show the user valid inputs
         printf("***Welcome to the ATM machine***\n");
         printf("Press 1 to check balance\n");
         printf("Press 2 to deposit\n");
         printf("Press 3 to withdraw\n");
         printf("Press X or x to exit\n:");
+
         // Taking the input into a character variable
         scanf(" %c", &ins);
+
         // Conditional statements that check the input to do different actions
         if (ins == 'x' || ins == 'X')
         {   
+
             //Helps the user break the program by using x or X
             break;
         }
         else if (ins == '1')
         {   
+
             // Shows the current balance
             printf("\nTHE BALANCE IS $ %d\n\n", check_balance(balance));
         }
         else if (ins == '2')
         {   
+
             // Helps to deposit
             printf("How much do you want to deposit: ");
             scanf("%d", &amount);
@@ -67,6 +75,7 @@ int main()
         }
         else if (ins == '3')
         {   
+            
             // Helps to withdraw
             printf("How much do you want to withdraw: ");
             scanf("%d", &amount);
